b105145be3dab3c79cb8ea0d950a4028e75901e1
hyde <hyde.song@yahoo.com>
2023-06-07 18:05:42 +0800

    fix

--- src/api/webhook.ts
+++ src/api/webhook.ts
@@ -6,8 +6,14 @@ import { DydxClient } from '@dydxprotocol/v3-client';
 const router = express.Router();
 
 const client = new DydxClient('https://api.stage.dydx.exchange', {
+  networkId: 5,
   apiTimeout: 3000,
   starkPrivateKey: '0057a33390995e610070f36eb6ca7e3d5c0b694018953e84a3c3668e59224675',
+  apiKeyCredentials: {
+    key: '4f0284f2-0b51-fd11-8e26-90f078b3a81c',
+    secret: 'DWJuAzsa0bg9DZiEqd-dPhru4WLIe65gSxPLf3c-',
+    passphrase: 'uv-R75sHJiuhZnUIZABl',
+  },
 });
 console.log(client);
 console.log(client.ethPrivate);
@@ -15,7 +21,7 @@ console.log(client.eth);
 console.log(client.onboarding);
 console.log(client.private);
 console.log(client.public);
-console.log(client.public.getMarkets);
+// console.log(client.public.getMarkets);
 console.log('testing....');
 // const request = axios.create({
 //   proxy: {
@@ -27,10 +33,8 @@ console.log('testing....');
 // request.get('https://api.stage.dydx.exchange/v3/markets').then(res => console.log('axios res....', res));
 
 router.post<{}>('/', async (req, res) => {
-    // res.end('hello world');
-  const market = await client.public.getMarkets();
-  console.log(market);
-  res.end(market);
+  console.log(req);
+  res.send(req.body);
 });
 
 export default router;
