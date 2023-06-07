d9bd39c09b20dd499f9df0b4efc2013bc1b498f7
hyde <hyde.song@yahoo.com>
2023-06-07 18:53:11 +0800

    goog

--- src/api/webhook.ts
+++ src/api/webhook.ts
@@ -1,4 +1,3 @@
-
 import express from 'express';
 import { DydxClient } from '@dydxprotocol/v3-client';
 // import axios from 'axios';
@@ -33,8 +32,32 @@ console.log('testing....');
 // request.get('https://api.stage.dydx.exchange/v3/markets').then(res => console.log('axios res....', res));
 
 router.post<{}>('/', async (req, res) => {
-  console.log(req);
-  res.send(req.body);
+  const { strategy, bar } = req.body;
+  console.log(strategy, bar);
+
+  const { account } = await client.private.getAccount('0xd98EFfF831aAa4Fe8834F9cb211d8397193A5492');
+  console.log('account', account);
+  const positionId = account.positionId;
+
+  if (req.body.strategy.side === 'BUY' || req.body.strategy.side === 'SELL') {
+    const myOrder = await client.private.createOrder({
+      market: strategy.market,
+      side: strategy.side,
+      type: strategy.type,
+      size: strategy.size,
+      price: strategy.price,
+      timeInForce: strategy.timeInForce,
+      postOnly: strategy.postOnly,
+      limitFee: strategy.limitFee,
+      expiration: strategy.expiration,
+    }, positionId);
+
+    res.send(`Order ${myOrder.order.id} has been created`);
+  } else {
+    await client.private.cancelAllOrders(strategy.market);
+    res.send('All orders canceled');
+  }
+  res.send('200 OK');
 });
 
 export default router;
