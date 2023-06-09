cc6fb6f3a2c848d75317543b7597d7b6218f15ea
Hyde Song <hyde.song@yahoo.com>
2023-06-09 01:17:40 +0000

    fix

--- src/api/webhook.ts
+++ src/api/webhook.ts
@@ -1,60 +1,24 @@
 import express from "express";
-import { DydxClient } from "@dydxprotocol/v3-client";
-// import Web3 from 'web3';
-// import axios from 'axios';
+import { Dydx } from "../services/index";
 
 const router = express.Router();
 
-const client = new DydxClient("https://api.stage.dydx.exchange", {
-  networkId: 5,
-  apiTimeout: 3000,
-  starkPrivateKey:
-    "0057a33390995e610070f36eb6ca7e3d5c0b694018953e84a3c3668e59224675",
-  apiKeyCredentials: {
-    key: "4f0284f2-0b51-fd11-8e26-90f078b3a81c",
-    secret: "DWJuAzsa0bg9DZiEqd-dPhru4WLIe65gSxPLf3c-",
-    passphrase: "uv-R75sHJiuhZnUIZABl",
-  },
-  // ethSendOptions: {},
-  // web3: Web3,
-  // web3Provider: undefined,
-  // timestampAdjustment: 1000,
-});
-// console.log(client);
-// console.log(client.ethPrivate);
-// console.log(client.eth);
-// console.log(client.onboarding);
-// console.log(client.private);
-// console.log(client.public);
-// console.log(client.public.getMarkets);
-// console.log("testing....");
-
-// client.public
-//   .getMarkets()
-//   .then((res) => console.log("DydxClient res....", res));
-
-// client.private
-//   .getAccount("0xd98EFfF831aAa4Fe8834F9cb211d8397193A5492")
-//   .then(async ({ account }) => {
-//     console.log("my account:", account);
-//     const positionId = account.positionId;
-//     console.log("my positionId:", positionId);
-//     const allPositions = await client.private.getPositions({});
-//     console.log("allPositions.....", allPositions);
-//   });
+const client = new Dydx();
 
 router.post<{}>("/", async (req, res) => {
-  console.log("req headers....", req.headers);
-  console.log("params....", req.body);
-
+  console.log("testing....");
+  client.webhook(req.body);
   // const { strategy, bar } = req.body;
   // console.log(strategy, bar);
 
-  // const { account } = await client.private.getAccount(
-  //   "0xd98EFfF831aAa4Fe8834F9cb211d8397193A5492"
-  // );
-  // console.log("account", account);
-  // const positionId = account.positionId;
+  const { account } = await client.private.getAccount(
+    "0xd98EFfF831aAa4Fe8834F9cb211d8397193A5492"
+  );
+  console.log("account", account);
+  const positionId = account.positionId;
+  console.log("my positionId:", positionId);
+  const allPositions = await client.private.getPositions({});
+  console.log("allPositions.....", allPositions);
 
   // if (req.body.strategy.side === "BUY" || req.body.strategy.side === "SELL") {
   //   const myOrder = await client.private.createOrder(
--- src/services/dydx.ts
+++ src/services/dydx.ts
@@ -1,23 +1,26 @@
-import { DydxClient } from '@dydxprotocol/v3-client';
+import { DydxClient } from "@dydxprotocol/v3-client";
+import { ClientOptions } from "@dydxprotocol/v3-client/build/src/dydx-client";
 
 export class Dydx extends DydxClient {
-  // constructor() {
-  //   super();
-  // }
+  constructor(
+    host: string = "https://api.stage.dydx.exchange",
+    options: ClientOptions = {
+      networkId: 5,
+      apiTimeout: 3000,
+      starkPrivateKey:
+        "0057a33390995e610070f36eb6ca7e3d5c0b694018953e84a3c3668e59224675",
+      apiKeyCredentials: {
+        key: "4f0284f2-0b51-fd11-8e26-90f078b3a81c",
+        secret: "DWJuAzsa0bg9DZiEqd-dPhru4WLIe65gSxPLf3c-",
+        passphrase: "uv-R75sHJiuhZnUIZABl",
+      },
+    }
+  ) {
+    super(host, options);
+  }
 
-  private webhook(data: any) {
-    console.log(this, 'this....');
-    console.log(data);
+  webhook(params: any) {
+    console.log(this, "this....");
+    console.log("params....", params);
   }
 }
-
-const client = new DydxClient('https://api.dydx.exchange');
-console.log(client);
-console.log(client.ethPrivate);
-console.log(client.eth);
-console.log(client.onboarding);
-console.log(client.private);
-console.log(client.public);
-console.log(client.public.getMarkets);
-console.log('testing....');
-client.public.getMarkets().then(res => console.log('client res....', res));
